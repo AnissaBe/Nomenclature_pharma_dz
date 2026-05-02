0a449bc507dee9fcba1d60d8d393fc2e28bb7f24
AnissaBe <104327459+AnissaBe@users.noreply.github.com>
2026-05-02 18:14:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ st.set_page_config(
     layout="wide"
 )
 
+password = st.text_input("Mot de passe", type="password")
+if password != "tonmotdepasse":
+    st.stop()
+    
 st.markdown("""
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap');
